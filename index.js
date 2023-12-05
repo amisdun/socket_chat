b74b3ca60df68c4e7db2ba8730ac96aa6c0a4ade
amisdun <michealdunamis@gmail.com>
2023-12-05 08:53:12 +0000

    amisdun: socket.io chat service

---
+++ index.js
@@ -0,0 +1,45 @@
+import express from 'express'
+import { createServer } from 'http'
+import { Server } from 'socket.io'
+
+const users = new Set()
+
+// start server and spin up socket
+const serverAndSocketConnector = () => {
+    const app = express()
+    const httpConnection = createServer(app)
+    const socket = new Server(httpConnection)
+
+    try {
+        // create a socket connection
+        socket.on('connection', (soc) => {
+            console.log('User has been connected')
+
+            // broad welcome message to all connected client
+            socket.local.emit('new user', 'Welcome to our chat channel')
+    
+            soc.on('new user', () => {
+                users.add(soc.id)
+                socket.local.emit('new user', `Welcome user ${soc.id}`)
+            })
+    
+            soc.on('message', (message) => {
+                console.log('Received message:', message);
+                socket.local.emit('message', message);
+            });
+    
+            soc.on('disconnect', () => {
+                console.log('User Disconnected')
+            })
+
+        })
+    } catch (error) {
+        throw new Error('An error has ocurred while trying to connect to the server')
+    }
+
+    httpConnection.listen(3001, () => {
+        console.log(`App listening to port 3001`)
+    })
+}
+
+serverAndSocketConnector()
